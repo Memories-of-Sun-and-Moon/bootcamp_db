e3899076206717f5b5347cac40352426dafb08ad
mimas <univ.t1c2ha3@gmail.com>
2023-08-22 10:50:45 +0900

    fix 3

--- sql/write_query/question3.sql
+++ sql/write_query/question3.sql
@@ -1,5 +1,6 @@
 -- 以下にクエリを書いてください
-SELECT users.username, posts.content, posts.posted_at FROM users
+SELECT users.username, posts.content, posts.posted_at
+FROM users
 JOIN posts ON users.id = posts.user_id
 ORDER BY posted_at DESC
 LIMIT 20;
